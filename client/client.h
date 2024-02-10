3d363ab2dab9b45a948b522e7ebcbe6882ac4e03
davidtey <davidteyby@gmail.com>
2024-02-11 02:58:03 +0800

    update client.h

--- client/client.h
+++ client/client.h
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdlib.h>
 
 using namespace std;
 
+int connectMenu();
+int mainMenu();
 int readInt(string prompt);
