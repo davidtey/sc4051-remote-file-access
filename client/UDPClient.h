28a8e8ed5057eb27b12d3d68ff0de9fb1619b3cc
davidtey <davidteyby@gmail.com>
2024-02-11 17:11:16 +0800

    initialise udpclient.h

---
+++ client/UDPClient.h
@@ -0,0 +1,28 @@
+#ifndef UDPCLIENT_H
+#define UDPCLIENT_H
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+
+using namespace std;
+
+class UDPClient{
+    private:
+        string serverIP;
+        int serverPort;
+        int sockfd;
+        int n;
+        int timeout;
+        struct sockaddr_in servaddr;
+        socklen_t servaddrLen = sizeof(servaddr);
+        char buffer[1024];
+
+    public:
+        UDPClient();
+        int connectServer(string ip, int port);
+        int send(char *msg);
+        int recv(char *buffer);
+};
+#endif
