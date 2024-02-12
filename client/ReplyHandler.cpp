3e08f00c4269bbffc685082198591764a16c71a2
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-12 19:48:45 +0800

    Create ReplyHandler.cpp

---
+++ client/ReplyHandler.cpp
@@ -0,0 +1,41 @@
+#include "ReplyHandler.h"
+
+using namespace std;
+
+namespace ReplyHandler{
+
+}
+int ReplyHandler::handleReply(char *b){
+    char *cur = b;
+    HandlerNum replyType = static_cast<HandlerNum>(utils::unmarshalInt(cur));
+
+    switch(replyType){
+        case READ_FILE_REPLY:
+            ReplyHandler::handleReadFileReply(b);
+            return 1;
+        case ERROR_REPLY:
+            ReplyHandler::handleErrorReply(b);
+            return 0;
+    }
+    return 1;
+}
+
+int ReplyHandler::handleReadFileReply(char *b){
+    char *cur = b + 4;
+    int fileLength = utils::unmarshalInt(cur);
+    cur += 4;
+    string fileContent = utils::unmarshalString(cur, fileLength);
+
+    cout << "File Content: \n" << fileContent << endl;
+
+    return 1;
+}
+
+int ReplyHandler::handleErrorReply(char *b){
+    char *cur = b + 4;
+    int errorLength = utils::unmarshalInt(cur);
+    cur += 4;
+    string errorString = utils::unmarshalString(cur, errorLength);
+    cout << "Error: " << errorString << endl;
+    return 1;
+}
