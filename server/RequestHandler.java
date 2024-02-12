88789ca23be8b0dcbdd464c45ed9591343ecaa3e
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-12 19:52:00 +0800

    bug fixes

--- server/RequestHandler.java
+++ server/RequestHandler.java
@@ -1,24 +1,26 @@
 package server;
 
-import java.net.SocketAddress;
+import java.net.InetAddress;
 
 /* Request Handler class (abstract class)
  * Unmarshals requests, invoke methods and marshals replies
  */
 
 public abstract class RequestHandler{
-    SocketAddress clientAddr;
+    InetAddress clientAddr;
     int clientPort;
+    int requestID;
     HandlerNum requestType;
     byte[] request;
+    byte[] reply;
 
-    public RequestHandler(SocketAddress addr, int port, byte[] req){
+    public RequestHandler(InetAddress addr, int port, int reqID, byte[] req){
         clientAddr = addr;
         clientPort = port;
+        requestID = reqID;
         request = req;
     }
 
     public abstract void unmarshalRequest();
     public abstract void executeRequest();
-    public abstract void marshalReply();
 }
