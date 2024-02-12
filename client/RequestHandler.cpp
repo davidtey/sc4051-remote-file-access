6e13397c9f9fc8814b9e3e78a53cf33bc153e535
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-12 19:49:24 +0800

    remove primitives in marshalling

--- client/RequestHandler.cpp
+++ client/RequestHandler.cpp
@@ -7,23 +7,14 @@ using namespace std;
  * Assumes buffer is big enough to contain message, else segmentation fault (handle buffer size in client)
  * Returns length of message
 */
-int RequestHandler::createReadRequest(string filePath, int offset, int numBytes, char *b){
+int RequestHandler::createReadRequest(int requestID, string filePath, int offset, int numBytes, char *b){
     char *cur = b; // index increment
 
-    utils::marshalInt(HandlerNum::READ_FILE_HANDLER, cur); // insert handler number into request message
+    utils::marshalInt(HandlerNum::READ_FILE_REQUEST, cur); // insert handler number into request message
     cur += 4;   // increment by 4 bytes
 
-    utils::marshalInt(3, cur);  // insert number of fields = 3 into request message
-    cur += 4;   // increment by 4 bytes
-
-    utils::marshalInt(PrimitiveNum::ENUM_STRING, cur);   // insert first argument (string) primitive number into request message
-    cur += 4;   // increment by 4 bytes
-
-    utils::marshalInt(PrimitiveNum::ENUM_INT, cur);     // insert second argument (int) primitive number into request message
-    cur += 4;   // increment by 4 bytes
-
-    utils::marshalInt(PrimitiveNum::ENUM_INT, cur);     // insert third argument (int) primitive number into request message
-    cur += 4;   // increment by 4 bytes
+    utils::marshalInt(requestID, cur);  // insert request ID
+    cur += 4;    // increment by 4 bytes
 
     utils::marshalInt(filePath.length(), cur);      // insert filePath string length into request message
     cur += 4;   // increment by 4 bytes
@@ -45,23 +36,14 @@ int RequestHandler::createReadRequest(string filePath, int offset, int numBytes,
  * Assumes buffer is big enough to contain message, else segmentation fault (handle buffer size in client)
  * Returns length of message
 */
-int RequestHandler::createWriteRequest(string filePath, int offset, string insertString, char *b){
+int RequestHandler::createWriteRequest(int requestID, string filePath, int offset, string insertString, char *b){
     char* cur = b; // char pointer
 
-    utils::marshalInt(HandlerNum::WRITE_FILE_HANDLER, cur); // insert handler number into request message
+    utils::marshalInt(HandlerNum::WRITE_FILE_REQUEST, cur); // insert handler number into request message
     cur += 4;   // increment by 4 bytes
 
-    utils::marshalInt(3, cur);  // insert number of fields = 3 into request message
-    cur += 4;   // increment by 4 bytes
-
-    utils::marshalInt(PrimitiveNum::ENUM_STRING, cur);   // insert first argument (string) primitive number into request message
-    cur += 4;   // increment by 4 bytes
-
-    utils::marshalInt(PrimitiveNum::ENUM_INT, cur);     // insert second argument (int) primitive number into request message
-    cur += 4;   // increment by 4 bytes
-
-    utils::marshalInt(PrimitiveNum::ENUM_STRING, cur);     // insert third argument (string) primitive number into request message
-    cur += 4;   // increment by 4 bytes
+    utils::marshalInt(requestID, cur);  // insert request ID
+    cur += 4;    // increment by 4 bytes
 
     utils::marshalInt(filePath.length(), cur);      // insert filePath string length into request message
     cur += 4;   // increment by 4 bytes
@@ -86,20 +68,14 @@ int RequestHandler::createWriteRequest(string filePath, int offset, string inser
  * Assumes buffer is big enough to contain message, else segmentation fault (handle buffer size in client)
  * Returns length of message
 */
-int RequestHandler::createMonitorRequest(string filePath, int monitorInterval, char *b){
+int RequestHandler::createMonitorRequest(int requestID, string filePath, int monitorInterval, char *b){
     char* cur = b; // char pointer
 
-    utils::marshalInt(HandlerNum::MONITOR_FILE_HANDLER, cur); // insert handler number into request message
+    utils::marshalInt(HandlerNum::MONITOR_FILE_REQUEST, cur); // insert handler number into request message
     cur += 4;   // increment by 4 bytes
 
-    utils::marshalInt(2, cur);  // insert number of fields = 2 into request message
-    cur += 4;   // increment by 4 bytes
-
-    utils::marshalInt(PrimitiveNum::ENUM_STRING, cur);   // insert first argument (string) primitive number into request message
-    cur += 4;   // increment by 4 bytes
-
-    utils::marshalInt(PrimitiveNum::ENUM_INT, cur);     // insert second argument (int) primitive number into request message
-    cur += 4;   // increment by 4 bytes
+    utils::marshalInt(requestID, cur);  // insert request ID
+    cur += 4;    // increment by 4 bytes
 
     utils::marshalInt(filePath.length(), cur);      // insert filePath string length into request message
     cur += 4;   // increment by 4 bytes
