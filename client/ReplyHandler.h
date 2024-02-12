1eba0882328b919163465990fca7eada0b42f528
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-12 19:48:48 +0800

    Create ReplyHandler.h

---
+++ client/ReplyHandler.h
@@ -0,0 +1,16 @@
+#ifndef REPLYHANDLER_H
+#define REPLYHANDLER_H
+#include <iostream>
+#include "utils.h"
+#include "HandlerNum.h"
+#include "PrimitiveNum.h"
+
+using namespace std;
+
+namespace ReplyHandler{
+    int handleReply(char *b);
+    int handleReadFileReply(char *b);
+    int handleErrorReply(char *b);
+}
+
+#endif
