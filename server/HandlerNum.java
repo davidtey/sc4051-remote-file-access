6c960a4aa40a58f569f8d708ba6d67fd0b97c347
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-12 19:51:34 +0800

    added int translation

--- server/HandlerNum.java
+++ server/HandlerNum.java
@@ -1,37 +1,68 @@
 package server;
 
 public enum HandlerNum {
-    READ_FILE_HANDLER,
-    WRITE_FILE_HANDLER,
-    MONITOR_FILE_HANDLER,
-    RESERVED_FILE_HANDLER1,
-    RESERVED_FILE_HANDLER2,
-    ERROR_HANDLER,
-    INSERTION_ACK,
-    NETWORK_PING,
-    NETWORK_ACK;
+    READ_FILE_REQUEST,          // 1
+    WRITE_FILE_REQUEST,         // 2
+    MONITOR_FILE_REQUEST,       // 3
+    RESERVED_FILE_REQUEST1,     // 4
+    RESERVED_FILE_REQUEST2,     // 5
+    READ_FILE_REPLY,            // 6
+    INSERTION_ACK,              // 7
+    NETWORK_PING,               // 8
+    NETWORK_ACK,                // 9
+    ERROR_REPLY;              // 10
 
     public static HandlerNum fromInt(int x){
         switch(x){
             case 1:
-                return READ_FILE_HANDLER;
+                return READ_FILE_REQUEST;
             case 2:
-                return WRITE_FILE_HANDLER;
+                return WRITE_FILE_REQUEST;
             case 3:
-                return MONITOR_FILE_HANDLER;
+                return MONITOR_FILE_REQUEST;
             case 4:
-                return RESERVED_FILE_HANDLER1;
+                return RESERVED_FILE_REQUEST1;
             case 5:
-                return RESERVED_FILE_HANDLER2;
+                return RESERVED_FILE_REQUEST2;
             case 6:
-                return ERROR_HANDLER;
+                return READ_FILE_REPLY;
             case 7:
                 return INSERTION_ACK;
             case 8:
                 return NETWORK_PING;
             case 9:
                 return NETWORK_ACK;
+            case 10:
+                return ERROR_REPLY;
         }
         return null;
     }
+
+    public static int toInt(HandlerNum handlerNum){
+        switch(handlerNum){
+            case READ_FILE_REQUEST:
+                return 1;
+            case WRITE_FILE_REQUEST:
+                return 2;
+            case MONITOR_FILE_REQUEST:
+                return 3;
+            case RESERVED_FILE_REQUEST1:
+                return 4;
+            case RESERVED_FILE_REQUEST2:
+                return 5;
+            case READ_FILE_REPLY:
+                return 6;
+            case INSERTION_ACK:
+                return 7;
+            case NETWORK_PING:
+                return 8;
+            case NETWORK_ACK:
+                return 9;
+            case ERROR_REPLY:
+                return 10;
+        }
+        return 0;
+    }
+
+
 }
