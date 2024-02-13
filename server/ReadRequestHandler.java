16eed6c929501ca1a77558e573dff2e6975e521b
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-14 04:25:50 +0800

    refactor

--- server/ReadRequestHandler.java
+++ server/ReadRequestHandler.java
@@ -1,70 +1,121 @@
 package server;
 
 import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ByteArrayOutputStream;
 
+/**Read Request Handler Class
+ * <pre>
+ * Handles read requests by unmarshalling request, invoking methods, marshalling reply and returning.
+ * Handles printing of requests and replies onto server console.
+ * </pre>
+ */
 public class ReadRequestHandler extends RequestHandler {
-    String filePath;
-    int offset;
-    int numBytes;
+    String filePath;            // request argument: file path
+    int offset;                 // request argument: offset
+    int numBytes;               // request argument: number of bytes to read
 
+    long serverLastModified;    // return: database file last modified time
+    int fileLength;             // return: database file length
+    int fileContentLength;      // return: requested database file content length 
+    byte[] fileContent;         // return: database file content
+
+    /**Constructor for ReadRequestHandler class
+     * @param addr client IP address
+     * @param port client port
+     * @param reqID request ID
+     * @param req byte array of incoming request
+     */
     public ReadRequestHandler(InetAddress addr, int port, int reqID, byte[] req){
         super(addr, port, reqID, req);
         requestType = HandlerNum.READ_FILE_REQUEST;
     }
 
+    /**Unmarshal read request according to predefined protocol.
+     */
     public void unmarshalRequest(){
-        int cur = 8;   // skip to first argument
-        int filePathLength = Utils.unmarshalInt(request, cur); // get filePath length
-        cur += 4;       // increment to start of filePath
-
-        filePath = Utils.unmarshalString(request, cur, filePathLength); // get filePath
-        cur += filePathLength; // increment to start of offset
-
-        offset = Utils.unmarshalInt(request, cur); // get offset
-        cur += 4;   // increment to start of numBytes
-
-        numBytes = Utils.unmarshalInt(request, cur);    // get numBytes
+        int filePathLength = Utils.unmarshalInt(request, 8); // get filePath length
+        filePath = Utils.unmarshalString(request, 12, filePathLength); // get filePath
+        offset = Utils.unmarshalInt(request, filePathLength + 12); // get offset
+        numBytes = Utils.unmarshalInt(request, filePathLength + 16);    // get numBytes
     }
 
+    /**Print read request on to server console.
+     */
+    public void printRequest(){
+        System.out.println("Read File Request from " + clientAddr + ":" + clientPort + "\nRequest ID: " + requestID + "\nFile path: " + 
+        filePath + "\nOffset: " + offset + "\nNumber of bytes to read: " + numBytes); 
+    }
+
+    /**Execute read request, invoke methods from Database.
+     */
     public void executeRequest(){
         try{
-            ServerFile serverFile = new ServerFile(filePath);
-            try{
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                byte[] fileData = serverFile.readFile(offset, numBytes);
-                byte[] header = new byte[8];
-                Utils.marshalInt(header, HandlerNum.toInt(HandlerNum.READ_FILE_REPLY), 0);  // add handler number to reply message
-
-                Utils.marshalInt(header, fileData.length, 4);
-
-                outputStream.write(header);
-                outputStream.write(fileData);
-
-                reply = outputStream.toByteArray();
-            }
-            catch (OutOfFileRangeException e){
-                String errorString = "File offset exceeds the file length. Offset: " + offset + " File length: " + 
-                serverFile.getFile().length();
+            filePath = Database.databasePath + filePath;                        // add database path in front of file path
+            serverLastModified = Database.getFileLastModified(filePath);        // get server last modified time
+            fileLength = Database.getFileLength(filePath);                      // get file length
+            fileContent = Database.readFromFile(filePath, offset, numBytes);    // get file content length
+            fileContentLength = fileContent.length;                             // get file content
+        }
+        catch (FileNotFoundException e){                                        // handle if file not found
+            errorEncountered = true;
+            errorMessage = "Error encountered! File at " + filePath + " could not be found.";
+            errorMessageLength = errorMessage.length();
+        }
+        catch (OutOfFileRangeException e){                                      // handle if offset exceed file length
+            errorEncountered = true;
+            errorMessage = "Error encountered! File offset exceeds the file length. Offset: " + offset + 
+            " File length: " + Database.getFileLength(filePath);
+            errorMessageLength = errorMessage.length();
+        }
+        catch (IOException e){                                                  // handle IOException
+            errorEncountered = true;
+            errorMessage = e.getMessage();
+            errorMessageLength = errorMessage.length();
+        }
+    }
     
-                reply = new byte[errorString.length() + 8];
-                Utils.marshalErrorString(reply, errorString);
-            }
+    /**Marshals read request reply
+     * <pre>
+     * Successful reply:
+     * int (4 bytes): READ_FILE_REPLY handler number
+     * long (8 bytes): server file last modified time
+     * int (4 bytes): file length
+     * int (4 bytes): file content length
+     * String (n bytes): file content
+     * 
+     * Error reply: 
+     * int (4 bytes): ERROR_REPLY handler number
+     * int (4 bytes): error message length
+     * String (n bytes): error message
+     * </pre>
+     */
+    public void marshalReply(){
+        if (!errorEncountered){     // successful reply
+            reply = new byte[fileContentLength + 20];
+            Utils.marshalInt(reply, HandlerNum.toInt(HandlerNum.READ_FILE_REPLY), 0);
+            Utils.marshalLong(reply, serverLastModified, 4);
+            Utils.marshalInt(reply, fileLength, 12);
+            Utils.marshalInt(reply, fileContentLength, 16);
+            Utils.marshalBytes(reply, fileContent, 20);
         }
-        catch (FileNotFoundException e){
-            String errorString = "File at " + filePath + " could not be found.";
-            reply = new byte[errorString.length() + 8];
-            Utils.marshalErrorString(reply, errorString);
-        }
-        catch (IOException e){
-            e.printStackTrace();
+        else{   // error reply
+            reply = new byte[errorMessageLength + 8];
+            Utils.marshalInt(reply, HandlerNum.toInt(HandlerNum.ERROR_REPLY), 0);
+            Utils.marshalInt(reply, errorMessageLength, 4);
+            Utils.marshalString(reply, errorMessage, 8);
         }
     }
 
-    public String toString(){
-        return "\nRead File Request from " + clientAddr + ":" + clientPort + "\nRequest ID: " + requestID + "\nFile path: " + filePath + 
-        "\nOffset: " + offset + "\nNumber of bytes to read: " + numBytes;
+    /**Print reply on to server console.
+     */
+    public void printReply(){   
+        if (!errorEncountered){     // successful reply
+            System.out.println("[To " + clientAddr + ":" + clientPort + "] " + new String(fileContent, StandardCharsets.UTF_8));
+        }
+        else{
+            System.err.println("[To " + clientAddr + ":" + clientPort + "] " + errorMessage);
+        }
     }
 }
