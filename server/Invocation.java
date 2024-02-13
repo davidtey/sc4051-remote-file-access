7abdbb5b90220b771e1c49c43b0bbcf22c944acc
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-14 06:01:46 +0800

    documentation

--- server/Invocation.java
+++ server/Invocation.java
@@ -1,5 +1,11 @@
 package server;
 
+/**Invocation Enum
+ * <pre>
+ * Invocation semantics enum, for better readability.
+ * </pre>
+ * 
+ */
 public enum Invocation {
     AT_LEAST_ONCE,
     AT_MOST_ONCE
