5e477f17aa5d8e82b4d4458866ea01357fc1644e
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-14 05:58:37 +0800

    documentation

--- server/CallbackRecord.java
+++ server/CallbackRecord.java
@@ -2,19 +2,28 @@ package server;
 import java.net.InetAddress;
 import java.util.Calendar;
 
+/**CallbackRecord class
+ * <pre>
+ * Record of the client's IP address, port and expiry time.
+ * </pre>
+ */
 public class CallbackRecord {
     InetAddress clientAddr;
     int port;
     Calendar expiryTime;
 
+    /**CallbackRecord constructor
+     * @param addr client IP address
+     * @param port client port
+     * @param expiry expiry time
+     */
     public CallbackRecord(InetAddress addr, int port, Calendar expiry){
         clientAddr = addr;
         this.port = port;
         expiryTime = expiry;
     }
 
-    /* Overload equals operator 
-     * Allows removal of CallbackRecord from list
+    /**Overload equals operator to facilitate removal of CallbackRecord from list
      */
     public boolean equals(CallbackRecord c){
         if (this.clientAddr == c.clientAddr && this.port == c.port && this.expiryTime == c.expiryTime){
@@ -25,6 +34,8 @@ public class CallbackRecord {
         }
     }
 
+    /**Overload printing function
+     */
     public String toString(){
         String out = "Client: " + clientAddr.toString() + ":" + String.valueOf(port) + " Expiry: " + expiryTime.getTime().toString(); 
         return out;
