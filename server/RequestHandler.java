e83a50464ce6389ba1bf29c1a41d41cb0f2bf45c
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-14 05:47:28 +0800

    refactor

--- server/RequestHandler.java
+++ server/RequestHandler.java
@@ -1,19 +1,35 @@
 package server;
 
+import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
 
-/* Request Handler class (abstract class)
- * Unmarshals requests, invoke methods and marshals replies
+/**Request Handler Class
+ * <pre>
+ * Abstract class. Unmarshals requests, invoke methods and marshals replies.
+ * Handles printing of requests and replies onto server console.
+ * </pre>
  */
-
 public abstract class RequestHandler{
-    InetAddress clientAddr;
-    int clientPort;
-    int requestID;
-    HandlerNum requestType;
-    byte[] request;
-    byte[] reply;
+    InetAddress clientAddr;             // Client IP address
+    int clientPort;                     // Client port
+    int requestID;                      // Request ID
+    HandlerNum requestType;             // Request Type
+    byte[] request;                     // Request buffer
+    byte[] reply;                       // Reply buffer
+    boolean errorEncountered = false;   // Error catch
+    int errorMessageLength;             // Error message length
+    String errorMessage = "";           // Error message
 
+    static List<RequestHandler> requestHistory = new ArrayList<RequestHandler>();   // Request history (list)
+
+    /**Constructor for RequestHandler class
+     * @param addr client IP address
+     * @param port client port
+     * @param reqID request ID
+     * @param req byte array of incoming request
+     */
     public RequestHandler(InetAddress addr, int port, int reqID, byte[] req){
         clientAddr = addr;
         clientPort = port;
@@ -21,6 +37,58 @@ public abstract class RequestHandler{
         request = req;
     }
 
+    /**Handle generic incoming request, sorts request type and calls the appropriate request handler to create a reply message
+     * @param request incoming request from UDP server
+     * @return reply message in the form of byte array
+     */
+    public static byte[] handleIncomingRequest(DatagramPacket request){
+        byte[] b = request.getData();                                                   // request byte array
+        HandlerNum requestType = HandlerNum.fromInt(Utils.unmarshalInt(b, 0));    // get request type from request
+        System.out.println("requestType: " + requestType);      // debug print
+
+        int requestID = Utils.unmarshalInt(b, 4);         // get request ID
+        System.out.println("requestID: " + requestID);          // debug print
+        RequestHandler requestHandler;
+
+        if (requestType == HandlerNum.READ_FILE_REQUEST){               // create read file request handler
+            requestHandler = new ReadRequestHandler(request.getAddress(), request.getPort(), requestID, b);
+        }
+        else if (requestType == HandlerNum.WRITE_FILE_REQUEST){         // create write file request handler
+            requestHandler = new WriteRequestHandler(request.getAddress(), request.getPort(), requestID, b);
+        }
+        else if (requestType == HandlerNum.MONITOR_FILE_REQUEST){       // create monitor file request handler
+            requestHandler = new MonitorRequestHandler(request.getAddress(), request.getPort(), requestID, b);
+        }
+        else if (requestType == HandlerNum.DELETE_FROM_FILE_REQUEST){   // create delete from file request handler
+            requestHandler = new DeleteFromFileRequestHandler(request.getAddress(), request.getPort(), requestID, b);
+        }
+        else if (requestType == HandlerNum.LIST_DIR_REQUEST){           // create list directory request handler
+            requestHandler = new ListDirRequestHandler(request.getAddress(), request.getPort(), requestID, b);
+        }
+        else if (requestType == HandlerNum.GET_FILE_ATTR_REQUEST){      // create get file attribute request handler
+            requestHandler = new GetFileAttrRequestHandler(request.getAddress(), request.getPort(), requestID, b);
+        }
+        else{
+            return null;
+        }
+
+        return requestHandler.handleRequest();                          // unmarshal request, invoke methods, marshal reply and return
+    }
+
+    /**Main function of read request handler, calls necessary procedures in order and returns reply message.
+     */
+    public byte[] handleRequest(){
+        unmarshalRequest();
+        printRequest();
+        executeRequest();
+        marshalReply();
+        printReply();
+        return reply;
+    }
+
     public abstract void unmarshalRequest();
+    public abstract void printRequest();
     public abstract void executeRequest();
+    public abstract void marshalReply();
+    public abstract void printReply();
 }
