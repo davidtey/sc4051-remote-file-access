6f655a042bb76fbedddd683216833ce96aa1a1c6
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-14 05:37:23 +0800

    refactor

--- server/DeleteFromFileRequestHandler.java
+++ server/DeleteFromFileRequestHandler.java
@@ -3,56 +3,102 @@ import java.net.InetAddress;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**Delete From File Request Handler Class
+ * <pre>
+ * Handles delete from file requests by unmarshalling request, invoking methods, marshalling reply and returning.
+ * Handles printing of requests and replies onto server console.
+ * </pre>
+ */
 public class DeleteFromFileRequestHandler extends RequestHandler{
-    String filePath;
-    int offset;
-    int numBytes;
+    String filePath;            // request argument: file path
+    int offset;                 // request argument: offset
+    int numBytes;               // request argument: number of bytes to delete
 
+    /**Constructor for DeleteFromFileRequestHandler class
+     * @param addr client IP address
+     * @param port client port
+     * @param reqID request ID
+     * @param req byte array of incoming request
+     */
     public DeleteFromFileRequestHandler(InetAddress addr, int port, int reqID, byte[] req){
         super(addr, port, reqID, req);
         requestType = HandlerNum.DELETE_FROM_FILE_REQUEST;
     }
 
+    /**Unmarshal delete from file request according to predefined protocol.
+     */
     public void unmarshalRequest(){
-        int cur = 8;   // skip to first argument
-        int filePathLength = Utils.unmarshalInt(request, cur); // get filePath length
-        cur += 4;       // increment to start of filePath
-
-        filePath = Utils.unmarshalString(request, cur, filePathLength); // get filePath
-        cur += filePathLength; // increment to start of offset
-
-        offset = Utils.unmarshalInt(request, cur); // get offset
-        cur += 4;   // increment to start of numBytes
-
-        numBytes = Utils.unmarshalInt(request, cur);    // get numBytes
+        int filePathLength = Utils.unmarshalInt(request, 8);            // get filePath length
+        filePath = Utils.unmarshalString(request, 12, filePathLength);  // get filePath
+        offset = Utils.unmarshalInt(request, filePathLength + 12);            // get offset
+        numBytes = Utils.unmarshalInt(request, filePathLength + 16);          // get numBytes
     }
 
+    /**Print delete from file request on to server console.
+     */
+    public void printRequest(){
+        System.out.println("[From " + clientAddr + ":" + clientPort + "] Delete From File Request #" + requestID + " from " + 
+        filePath + " with " + offset + " bytes offset " + " for " + numBytes + " bytes."); 
+    }
+
+    /**Execute delete from file request, invoke methods from Database.
+     */
     public void executeRequest(){
         try{
-            ServerFile serverFile = new ServerFile(filePath);
-            try{
-                serverFile.delete(offset, numBytes);
-                reply = new byte[4];
-                Utils.marshalInt(reply, HandlerNum.toInt(HandlerNum.DELETE_FROM_FILE_ACK), 0);
-                MonitorRequestHandler.notifyUpdate(filePath, serverFile);
-            }
-            catch (OutOfFileRangeException e){
-                String errorString = "File offset exceeds the file length. Offset: " + offset + " File length: " + 
-                serverFile.getFile().length();
-    
-                reply = new byte[errorString.length() + 8];
-                Utils.marshalErrorString(reply, errorString);
-            }
+            String fullFilePath = Database.databasePath + filePath;                 // add database path in front of file path         
+            Database.deleteFromFile(fullFilePath, offset, numBytes);                // delete from file
         }
-        catch (FileNotFoundException e){
-            String errorString = "File at " + filePath + " could not be found.";
-            reply = new byte[errorString.length() + 8];
-            Utils.marshalErrorString(reply, errorString);
+        catch (FileNotFoundException e){                                        // handle if file not found
+            errorEncountered = true;
+            errorMessage = "Error encountered! File at " + filePath + " could not be found.";
+            errorMessageLength = errorMessage.length();
+        }
+        catch (OutOfFileRangeException e){                                      // handle if offset exceed file length
+            errorEncountered = true;
+            errorMessage = "Error encountered! File offset exceeds the file length. Offset: " + offset + 
+            " File length: " + Database.getFileLength(filePath);
+            errorMessageLength = errorMessage.length();
+        }
+        catch (IOException e){                                                  // handle IOException
+            errorEncountered = true;
+            errorMessage = e.getMessage();
+            errorMessageLength = errorMessage.length();
+        }
+        
+    }
+
+    /**Marshals delete from file request reply
+     * <pre>
+     * Successful reply:
+     * int (4 bytes): DELETE_FROM_FILE_ACK handler number
+     * 
+     * Error reply: 
+     * int (4 bytes): ERROR_REPLY handler number
+     * int (4 bytes): error message length
+     * String (n bytes): error message
+     * </pre>
+     */
+    public void marshalReply(){
+        if (!errorEncountered){     // successful reply
+            reply = new byte[4];
+            Utils.marshalInt(reply, HandlerNum.toInt(HandlerNum.DELETE_FROM_FILE_ACK), 0);
+        }
+        else{                       // error reply
+            reply = new byte[errorMessageLength + 8];
+            Utils.marshalInt(reply, HandlerNum.toInt(HandlerNum.ERROR_REPLY), 0);
+            Utils.marshalInt(reply, errorMessageLength, 4);
+            Utils.marshalString(reply, errorMessage, 8);
         }
     }
 
-    public String toString(){
-        return "\nDelete from File Request from " + clientAddr + ":" + clientPort + "\nRequest ID: " + requestID + "\nFile path: " + filePath + 
-        "\nOffset: " + offset + "\nNumber of bytes to delete: " + numBytes;
+    /**Print reply on to server console.
+     */
+    public void printReply(){   
+        if (!errorEncountered){     // successful reply
+            System.out.println("[To " + clientAddr + ":" + clientPort + "] " + "DELETE_FROM_FILE_ACK");
+        }
+        else{
+            System.err.println("[To " + clientAddr + ":" + clientPort + "] " + errorMessage);
+        }
     }
 }
