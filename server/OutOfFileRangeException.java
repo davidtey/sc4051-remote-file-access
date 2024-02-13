d1d9b842f61476c30d8468a3392cf513255e51a2
davidtey <64002422+davidtey@users.noreply.github.com>
2024-02-14 06:04:02 +0800

    documentation

--- server/OutOfFileRangeException.java
+++ server/OutOfFileRangeException.java
@@ -1,6 +1,21 @@
 package server;
 
+/**OutOfFileRangeException
+ * <pre>
+ * Used to catch instances where client tries to read byte that is outside of file range.
+ * </pre>
+ * 
+ */
 public class OutOfFileRangeException extends Exception{
+    /**Empty constructor
+     */
+    public OutOfFileRangeException (){
+        super();
+    }
+
+    /**Constructor with error message
+     * @param errorMessage error message
+     */
     public OutOfFileRangeException (String errorMessage){
         super(errorMessage);
     }
